e05638b9a5988d5855d96d92fe14585c0252e9a3
QUEEN-DIANA <dianadenzyy@gmail.com>
2026-04-26 13:19:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ module.exports = {
     AUTO_RECORDING: process.env.AUTO_RECORDING || "false",      // Auto-record voice notes?
     AUTO_TYPING: process.env.AUTO_TYPING || "false",            // Show typing indicator?
     MENTION_REPLY: process.env.MENTION_REPLY || "false",   // reply on mentioned message 
-    MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://files.catbox.moe/2rrybz.jpg",  // Bot's "alive" menu mention image
+    MENU_IMAGE_URL: getConfig("MENU_IMAGE_URL") || "https://i.ibb.co/rRdvY8zL/upload-1777203469367-a36a6cfb-jpg.jpg",  // Bot's "alive" menu mention image
 
     // ===== SECURITY & ANTI-FEATURES =====
     ANTI_DELETE: process.env.ANTI_DELETE || "true", // true antidelete to recover deleted messages 
